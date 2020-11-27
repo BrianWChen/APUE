eba3281e69a524066894dc85fcfd72098a0fc09e
BrianWChen <brian.c@163.com>
2020-11-27 17:05:29 +0800

    chkpass glob mydu username

--- fs/glob.c
+++ fs/glob.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <glob.h>
 
-#define PAT "/etc/a*.conf"
+//#define PAT "/etc/a*.conf"
+#define PAT "/etc/*"
 
 /* int errfunc_(const char *errpath, int errno)
 {
@@ -14,18 +15,17 @@
 int main()
 {
 	glob_t globres;
-	int err;
+	int i, err;
 
-    err = glob(PAT, 0. NULL, &globres);
+    err = glob(PAT, 0, NULL, &globres);
     if (err)
     {
     	printf("Error code = %d\n", err);
     	exit(1);
     }
 
-    for (int i = 0; i < globres.gl_pathc; i++)
+    for (i = 0; i < globres.gl_pathc; i++)
     {
-
     	puts(globres.gl_pathv[i]);
     }
 
--- fs/mydu.c
+++ fs/mydu.c
@@ -3,10 +3,32 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <unistd.h>
+#include <glob.h>
+#include <string.h>
+
+#define PATHSIZE 1024
+
+static int path_noloop(const char *path)
+{
+    char *pos;
+
+    pos = strrchr(path, '/');
+    if (pos == NULL)
+        exit(1);
+
+    if (strcmp(pos+1, ".") == 0 || strcmp(pos+1, "..") == 0)
+        return 0;
+
+    return 1;
+}
 
 static int64_t mydu(const char *path)
 {
-	struct stat statres;
+	static struct stat statres;
+    static char nextpath[PATHSIZE];
+    glob_t globres;
+    int64_t sum;
+    int i;
 
 	if (lstat(path, &statres) < 0)
 	{
@@ -16,10 +38,30 @@ static int64_t mydu(const char *path)
 
 	if (!S_ISDIR(statres.st_mode))
 	{
-		return statres.st_blocks / 2;
+		return statres.st_blocks;
 	}
 
-	return 0;
+    strncpy(nextpath, path, PATHSIZE);
+    strncat(nextpath, "/*", PATHSIZE);
+    glob(nextpath, 0, NULL, &globres);
+
+    strncpy(nextpath, path, PATHSIZE);
+    strncat(nextpath, "/.*", PATHSIZE);
+    glob(nextpath, GLOB_APPEND, NULL, &globres);
+
+    sum = statres.st_blocks;
+
+    for (i = 0; i < globres.gl_pathc; i++)
+    {
+        if (path_noloop(globres.gl_pathv[i]))
+        {
+            sum += mydu(globres.gl_pathv[i]);
+        }
+    }
+
+    globfree(&globres);
+
+    return sum;
 }
 
 int main(int argc, char **argv)
@@ -30,7 +72,7 @@ int main(int argc, char **argv)
         exit(1);
     }
 
-    printf("%ll\n", mydu(argv[1]));
+    printf("%lld\n", mydu(argv[1]) / 2);
 
     exit(0);
 }
